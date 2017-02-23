7103fa4ae0463f16fdfe8081c369794d0032e3c1
ZHERYU <eric.zhou.us@gmail.com>
2017-02-23 14:45:02 -0800

    initial commit. Nothing much to see here

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController: MonoBehaviour {
+
+    public GameObject target;
+    Camera thisCamera;
+    public Vector3 cameraAngle;
+    Vector3 defaultCameraAngle = new Vector3();
+	// Use this for initialization
+	void Start () {
+        thisCamera = gameObject.GetComponent<Camera>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

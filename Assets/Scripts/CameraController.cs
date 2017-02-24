f2f588db832d77044f52a31fd3e06a1e42768bc6
ZHERYU <eric.zhou.us@gmail.com>
2017-02-23 16:15:34 -0800

    finished camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -33,6 +33,8 @@ public class CameraController: MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
-		
-	}
+        Vector3 targetCamPos = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
 }

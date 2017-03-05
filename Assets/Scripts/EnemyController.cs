0cd39b5074da45ef6ef8d6775941cecf8ace4b41
ZHERYU <eric.zhou.us@gmail.com>
2017-03-05 00:00:47 -0800

    enemycontroller changes

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,14 +6,21 @@ using UnityEngine;
 [RequireComponent(typeof(EnemyMovement))]
 public class EnemyController : MonoBehaviour {
 
+    public Rigidbody rb { get; set;}
     EnemyState currentState;
     EnemyState nextState;
     
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        if (rb == null)
+        {
+            rb = this.transform.root.gameObject.GetComponent<Rigidbody>();
+        }
+        rb.freezeRotation = true;
+
+        currentState = new EnemyMovement.Idle(this);
+    }
 	
 	// Update is called once per frame
 	void Update () {

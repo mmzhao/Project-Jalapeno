043e21897d551f31cba069769e4304495fa3a903
Jason Hu <jasonhu@berkeley.edu>
2017-03-07 16:36:32 -0800

    created attack structure

---
+++ Assets/Scripts/Attack1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack1 : PlayerAttack {
+    
+    /** Fields from PlayerAttack:
+     * PlayerController pc;
+     * Collider[] hitboxes;
+     */
+
+    public Attack1(PlayerController controller)
+    {
+        pc = controller;
+    }
+
+    // Create hitboxes, start animation
+    public override void Enter();
+
+    public override void FixedUpdate();
+
+    public override void Update();
+
+    // Destroy hitboxes
+    public override void Exit();
+}

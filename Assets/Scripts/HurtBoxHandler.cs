4cfd2d63fcc319453228aaf6e07f2a269dfc10a4
ZHERYU <eric.zhou.us@gmail.com>
2017-04-04 17:31:49 -0700

    hurtbox and hitbox tags and layers

---
+++ Assets/Scripts/HurtBoxHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Created by Eric Zhou
+ * Handles triggers for hurtboxes and passes them to PlayerController
+ * Handles Hurtbox initialization for further idiotproofing
+ */
+public class HurtBoxHandler : MonoBehaviour {
+    public PlayerController pc;
+
+    void Awake ()
+    {
+        pc = transform.root.GetComponent<PlayerController>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        pc.getHit(gameObject, other);
+    }
+}

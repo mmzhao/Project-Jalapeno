629bf178570b5d0e93d2c4d361e6e63fc61c89c3
Michael Zhao <mengzhe.zhao@gmail.com>
2017-04-25 18:07:15 -0700

    enemy attack ish

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -7,62 +7,89 @@ public class EnemyAttack : MonoBehaviour {
 
 	public class Attack : EnemyState
 	{
+		BoxCollider hitbox;
+		GameObject[] hitboxes;
+		float[] activateHitboxMoments = { 0f }; // these mark the timestamp to move to the next hitbox
+		float[] deactivateHitboxMoments = { .5f};
+		Vector3 facing;
+		int numHitboxes;
+		int activateHitboxIndex = 0;
+		int deactivateHitboxIndex = 0;
+		int damage;
+		protected float attackDuration;
+		protected float attackTimer;
+
 		EnemyController ec;
-		Vector3 attackDir;
-		public float attackRange;
-		int donecount = 5;
-		int counter;
+
+		protected static string HITBOX_CONTAINER_TAG = "HitboxContainer";
+
+		// make a cube to show hitbox
+		//	GameObject myCube;
 		GameObject attack;
 
 		public Attack(EnemyController enemyController, Vector3 dir)
 		{
 			this.ec = enemyController;
-			attackDir = dir;
-			attackRange = enemyController.attackRange;
-			counter = 0;
-			ec.lastAttackTime = ec.attackRechargeTime;
+			facing = dir.normalized;
+			facing.y = 0;
+			attackDuration = 0.5f;
+			attackTimer = 0;
+			numHitboxes = activateHitboxMoments.Length;
+			hitboxes = new GameObject[numHitboxes];
+			damage = 30;
 		}
 
 		public override void Enter()
 		{
-			attack = (GameObject)Instantiate (ec.projectile);
-			attack.transform.position = ec.transform.position + attackDir.normalized*8;
-//			attack.transform.localEulerAngles = new Vector3 (0, -Mathf.Atan2 (attackDir.z, attackDir.x) * 180f / Mathf.PI, 0);
-			attack.GetComponent<ProjectileController>().dir = attackDir;
-//			foreach (Transform hitbox in attack.transform) 
-//			{
-//				hitbox.gameObject.SetActive (false);
-//                hitbox.GetComponent<Collider>().isTrigger = false;
-//			}
+			attack = (GameObject) GameObject.Instantiate(ec.ap);
+			damage = attack.GetComponent<AttackVariables>().Damage();
+			attack.transform.parent = ec.transform;
+			attack.transform.position = ec.transform.position + new Vector3(0, 2, 0) + facing.normalized*8;
+			attack.transform.localEulerAngles = new Vector3 (0, -Mathf.Atan2 (facing.z, facing.x) * 180f / Mathf.PI, 0);
+			Transform hitboxContainer = findHitboxesByTag(attack.transform);
+			for (int i = 0; i < numHitboxes; i++)
+			{
+				hitboxes[i] = hitboxContainer.GetChild(i).gameObject;
+				hitboxes[i].SetActive(false);
+			}
 		}
 
+		protected Transform findHitboxesByTag(Transform attackPrefab)
+		{
+			foreach (Transform child in attackPrefab)
+			{
+				if (child.tag == HITBOX_CONTAINER_TAG)
+				{
+					return child;
+				}
+			}
+			return null;
+		}
+
+		// Destroy hitboxes
 		public override void Exit()
 		{
-//			Destroy(attack);
+//			GameObject.Destroy(attack);
 		}
 
 		public override void FixedUpdate()
 		{
-//			int hitboxIndex = 0;
-//			foreach (Transform hitbox in attack.transform) 
-//			{
-////				Debug.Log (curMoves + " " + numMoves + " " + curMoves / (numMoves / 5));
-//				if (counter / (donecount / 5) == hitboxIndex) {
-////					Debug.Log (hitboxIndex + " True");
-//					hitbox.gameObject.SetActive (true);
-//				} else {
-////					Debug.Log (hitboxIndex + " False");
-//					hitbox.gameObject.SetActive (false);
-//				}
-//				hitboxIndex++;
-//			}
-//
-			counter++;
-			//			Debug.Log ("attack " + curMoves);
-//			GameObject.FindGameObjectWithTag ("Player").GetComponent<Health> ().TakeDamage (1);
-
-			if (counter >= donecount) 
+			//			base.FixedUpdate();
+			attackTimer += Time.deltaTime;
+			while (activateHitboxIndex < numHitboxes && attackTimer >= activateHitboxMoments[activateHitboxIndex])
 			{
+				hitboxes[activateHitboxIndex].SetActive(true);
+				activateHitboxIndex++;
+			}
+
+			while (deactivateHitboxIndex < numHitboxes && attackTimer >= deactivateHitboxMoments[deactivateHitboxIndex])
+			{
+				hitboxes[deactivateHitboxIndex].SetActive(false);
+				deactivateHitboxIndex++;
+			}     
+			if (attackTimer > attackDuration) 
+			{
+				attack.GetComponent<ProjectileController>().dir = facing;
 				ec.nextState = new EnemyMovement.Targeting (ec);
 			}
 		}

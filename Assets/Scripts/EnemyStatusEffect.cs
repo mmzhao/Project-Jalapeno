24d4725db103dd13941439284f3d63478e334e4d
ZHERYU <eric.zhou.us@gmail.com>
2017-05-01 19:45:57 -0700

    enemy status effect

---
+++ Assets/Scripts/EnemyStatusEffect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStatusEffect : MonoBehaviour {
+
+    public class HitStun : EnemyState
+    {
+        public override void Enter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Exit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void FixedUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

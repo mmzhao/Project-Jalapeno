368af04ee887aa58312ccbc0bb6c076504605301
Jason Hu <jasonhu@berkeley.edu>
2017-05-01 17:55:33 -0700

    boss scripts...?

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -5,101 +5,97 @@ using UnityEngine.AI;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(PlayerMovement))]
-public class BossController : EnemyController {
+public class BossController : MonoBehaviour {
+    BossStates currentState;
+    public BossStates nextState;
+    public Rigidbody rb;
+    public bool stateEnded { get; set; }
+    public NavMeshAgent navAgent { get; set; }
+    public Animator anim;
+    
+    // These variables manage movement.
+    public float maxSpeed; // Lets us set the maximum speed.
 
-//	EnemyState currentState;
-//	public EnemyState nextState;
-//	public Vector3 movement { get; set; }
-//	public Rigidbody rb;
-//	public bool stateEnded { get; set; }
-//	public float maxSpeed;
-//	public float curSpeed { get; set; }
-//	public float targetRange;
-//	public float attackRange;
-//	public NavMeshAgent navAgent { get; set; }
-//	public GameObject ap;
-//	public Animator anim;
-//
-//	// render latch circle
-//	GameObject latchRadius;
-//	GameObject attackRadius;
-//	public Vector3 lastPlayerPos;
-//	public bool hasLastPlayerPos;
+    // These variables manage resets between modes.
+    public int mode; // 0 = Motion-sensing; 1 = Searchlight. Keeps track of mode.
+    public int numResets; // Keeps track of the number of times the boss has reset to motion-sensing mode.
+    public int initialSpawn; // Sets how many minons spawn the very first time. 
+    public int minionScaling; // Sets how many extra minions appear upon each reset.
+    public int speedScaling; // Sets how much faster the boss can move upon each reset.
 
-	void Awake ()
-	{
-		maxSpeed = 20.0f;
-		targetRange = 150.0f;
-		attackRange = 60.0f;
-		hasLastPlayerPos = false;
-		//variable initializations
-		GameObject rootParent = this.transform.gameObject;
-		if (rb == null)
-		{
-			rb = rootParent.GetComponent<Rigidbody>();
-		}
-		if (navAgent == null)
-		{
-			navAgent = rootParent.GetComponent<NavMeshAgent>();
-		}
-		if (anim == null)
-		{
-			anim = rootParent.GetComponent<Animator>();
-		}
-	}
+    // These variables manage detection.
+    public float detectlimit; // Sets how long the player must be undetected for boss to switch to searchlight mode.
+    public bool detected; // Indicates whether or not player has been detected.
+    public float detectTime; // Keeps track of how long it has been since player was last detected (requires detected to be false).
 
-//	// Use this for initialization
-//	void Start () {
-//		//		rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
-//		rb.constraints = RigidbodyConstraints.FreezeRotation;
-//		currentState = new EnemyMovement.Targeting(this);
-//
-//		latchRadius = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-//		Renderer ren = latchRadius.GetComponents<Renderer> () [0];
-//		ren.material.color = Color.green;
-//		latchRadius.transform.localScale = new Vector3 (2*targetRange, .1f, 2*targetRange);
-//		Destroy(latchRadius.GetComponent<SphereCollider> ());
-//
-//		attackRadius = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-//		Renderer ren2 = attackRadius.GetComponents<Renderer> () [0];
-//		ren2.material.color = Color.red;
-//		attackRadius.transform.localScale = new Vector3 (2*attackRange, .1f, 2*attackRange);
-//		Destroy(attackRadius.GetComponent<SphereCollider> ());
-//	}
-//
-//	// Update is called once per frame
-//	void Update () {
-//
-//	}
-//
-//	void FixedUpdate()
-//	{
-//		latchRadius.transform.position = gameObject.transform.position + new Vector3(0, .2f, 0);
-//
-//		attackRadius.transform.position = gameObject.transform.position + new Vector3(0, .4f, 0);
-//
-//		//		Debug.Log(GameObject.FindGameObjectWithTag ("Player").GetComponent<Health>().currentHealth);
-//		//		Debug.Log(currentState);
-//		currentState.FixedUpdate();
-//		if (nextState != null)
-//		{
-//			stateEnded = false;
-//			currentState.Exit();
-//			currentState = nextState;
-//			nextState = null;
-//			currentState.Enter();
-//		}
-//	}
-//
-//	void OnTriggerEnter (Collider other)
-//	{
-//		//		Debug.Log ("e");
-//		//		Debug.Log (other.gameObject.transform.parent.name.Substring(0, 11));
-//		if (other.gameObject.transform.parent.name.Length >= 9 &&
-//			other.gameObject.transform.parent.name.Substring(0, 8) == "MCAttack")
-//		{
-//			//			Debug.Log ("hit " + other.gameObject.name);
-//			this.transform.gameObject.GetComponent<Health> ().TakeDamage (1);
-//		}
-//	}
+    void Awake()
+    {
+        maxSpeed = 35;
+        mode = 0;
+        numResets = 0;
+        initialSpawn = 4;
+        minionScaling = 2;
+        speedScaling = 5;
+  
+        GameObject rootParent = this.transform.gameObject;
+        if (rb == null)
+        {
+            rb = rootParent.GetComponent<Rigidbody>();
+        }
+        if (navAgent == null)
+        {
+            navAgent = rootParent.GetComponent<NavMeshAgent>();
+        }
+        if (anim == null)
+        {
+            anim = rootParent.GetComponent<Animator>();
+        }
+    }
+
+    void Start()
+    {
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
+        currentState = new Moving(this);
+    }
+
+    void Update() {; }
+    void FixedUpdate()
+    {
+        rb.velocity = Vector3.zero;
+        currentState.FixedUpdate();
+        if (nextState != null)
+        {
+            stateEnded = false;
+            currentState.Exit();
+            currentState = nextState;
+            nextState = null;
+            currentState.Enter();
+        }
+        if (this.gameObject.GetComponent<Health>().currentHealth <= 0)
+        {
+            currentState.Exit();
+            nextState = null;
+            currentState = new Dying(this);
+        }
+    }
+
+    public void getHit(GameObject go, Collider other)
+    {
+        if (mode == 1)
+        {
+            if (other.gameObject.transform.root.tag == "Player")
+            {
+                Health h = GetComponent<Health>();
+                Transform t = other.transform;
+                while (t.parent != t.root) t = t.parent;
+
+                int dmg = t.GetComponent<AttackVariables>().Damage();
+                h.TakeDamage(dmg, t);
+            }
+        }
+    }
+
+    // Spawns numSpawn number of enemies near the boss.
+    public void SpawnEnemies(int numSpawn) {; }
 }
---
+++ Assets/Scripts/BossStates.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BossStates {
+    public BossController bc;
+    public GameObject player;
+
+    public abstract void Enter();
+    public abstract void FixedUpdate();
+    public abstract void Update();
+    public abstract void Exit();
+}
+
+
+public class Moving : BossStates
+{
+    // Maximum speed that boss can move.
+    float maxSpeed;
+    // Current speed that boss is moving.
+    float curSpeed;
+
+    public Moving(BossController mybc)
+    {
+        bc = mybc;
+        player = GameObject.FindGameObjectWithTag("Player");
+        maxSpeed = mybc.maxSpeed;
+        curSpeed = 0;
+    }
+    public override void Enter() {; }
+    public override void FixedUpdate() {; }
+    public override void Update() {; }
+    public override void Exit() {; }
+
+    public void SearchForTarget(int mode) {; }
+
+    public void Move(Vector3 d) {; } 
+}
+
+public class Attacking : BossStates
+{
+    public Attacking(BossController mybc)
+    {
+        bc = mybc;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    public override void Enter() {; }
+    public override void FixedUpdate() {; }
+    public override void Update() {; }
+    public override void Exit() {; }
+
+    public void Attack(int mode) {; }
+}
+
+public class Transitioning : BossStates
+{
+    // numSpawn indicates how many new enemies to spawn when transitioning from Searchlight back to Motion-Sensing.
+    public int numSpawn; 
+    public Transitioning(BossController mybc)
+    {
+        bc = mybc;
+        player = GameObject.FindGameObjectWithTag("Player");
+        numSpawn = 0;
+    }
+    public override void Enter()
+    {
+        if (bc.mode == 1)
+        {
+            numSpawn = bc.initialSpawn + bc.minionScaling * bc.numResets;
+            bc.SpawnEnemies(numSpawn);
+        }
+    }
+    public override void FixedUpdate() {; }
+    public override void Update() {; }
+    public override void Exit() {; }
+}
+
+public class Dying : BossStates
+{
+    public Dying(BossController mybc)
+    {
+        bc = mybc;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    public override void Enter() {; }
+    public override void FixedUpdate() {; }
+    public override void Update() {; }
+    public override void Exit() {; }
+}
+

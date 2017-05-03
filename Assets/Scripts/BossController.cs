a614895bc5b79d4a18f3bb81a8dfd47f612d4c6a
Michael Zhao <mengzhe.zhao@gmail.com>
2017-05-02 17:16:55 -0700

    boss looking places

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -57,7 +57,7 @@ public class BossController : MonoBehaviour {
         detectTime = 0;
         // Initialize searchlight variables.
         facing = Direction.S;
-        facingLimit = 0.5f;
+        facingLimit = 1.5f;
         facingTime = 0;
         fieldOfView = 0.79f;
         hits = 0;
@@ -90,12 +90,29 @@ public class BossController : MonoBehaviour {
     void Update() {; }
     void FixedUpdate()
     {
+		for (int i = 0; i <= 8; i++) {
+			gameObject.transform.GetChild (i).gameObject.SetActive (false);
+		}
 		if (mode == 0) {
 			gameObject.transform.GetChild (0).gameObject.SetActive (true);
-			gameObject.transform.GetChild (1).gameObject.SetActive (false);
 		} else if (mode == 1) {
-			gameObject.transform.GetChild (0).gameObject.SetActive (false);
-			gameObject.transform.GetChild (1).gameObject.SetActive (true);
+			if (facing == Direction.S) {
+				gameObject.transform.GetChild (1).gameObject.SetActive (true);
+			} else if (facing == Direction.SW) {
+				gameObject.transform.GetChild (2).gameObject.SetActive (true);
+			} else if (facing == Direction.W) {
+				gameObject.transform.GetChild (3).gameObject.SetActive (true);
+			} else if (facing == Direction.NW) {
+				gameObject.transform.GetChild (4).gameObject.SetActive (true);
+			} else if (facing == Direction.N) {
+				gameObject.transform.GetChild (5).gameObject.SetActive (true);
+			} else if (facing == Direction.NE) {
+				gameObject.transform.GetChild (6).gameObject.SetActive (true);
+			} else if (facing == Direction.E) {
+				gameObject.transform.GetChild (7).gameObject.SetActive (true);
+			} else if (facing == Direction.SE) {
+				gameObject.transform.GetChild (8).gameObject.SetActive (true);
+			} 
 		}
 //		Debug.Log (currentState);
         rb.velocity = Vector3.zero;
